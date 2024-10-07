89db1236c991ce8ce8efc2818fb101d5b89f7f92
gojinkyu <jhk070113@naver.com>
2024-10-07 22:53:06 +0900

    chw

---
+++ hw4.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void) {
+	int a,b,c,d=0;
+	
+	printf("양의 정수 입력: ");
+	scanf("%d", &a);
+
+	for (b = 1; b <= a; b++) {
+		for (c = 1; c <= b; c++) {
+			if (d > 2) {
+				printf("“It is not a prime number.");
+			}
+			if (b % c == 0) {
+				d += 1;
+			}
+		}
+		if (d == 2) {
+			printf("It is a prime number.");
+		}
+		d = 0;
+	}
+
+
+	return 0;
+}
