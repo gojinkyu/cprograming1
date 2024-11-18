ecc723ae1e11e056fe1638d199f4478c6c6b1e10
gojinkyu <jhk070113@naver.com>
2024-11-19 00:15:51 +0900

    hw8

---
+++ hw8.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h> 
+#include<math.h>
+
+float calculateSD(float data[]);
+
+int main() 
+{
+    int i;
+    float data[5];
+    printf("5개의 요소 입력: ");
+    for (i = 0; i < 5; ++i)
+        scanf("%f", &data[i]);
+    printf("\n표준 편차 = %.6f", calculateSD(data));
+    return 0;
+}
+
+float calculateSD(float data[]) 
+{
+    float sum = 0.0, mean, SD = 0.0;
+    int i;
+    for (i = 0; i < 5; ++i) {
+        sum += data[i];
+    }
+    mean = sum / 5;
+    for (i = 0; i < 5; ++i)
+        SD += pow(data[i] - mean, 2);
+    return sqrt(SD / 5);
+}
